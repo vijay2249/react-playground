501e222bfa0be36638807ff8528bc835faedfd02
VIJAY 'H'CN <vijaychowdary3699@gmail.com>
2022-10-12 18:05:56 +0530

    users project done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+import React, { useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const handleAddUser = user =>{
+    setUsers((prevState) =>{
+      return [...prevState, user]
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AddUser onAddUser={handleAddUser}/>
+      <UsersList users={users}/>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import styles from './Card.module.css'
+
+export default function Card(props){
+  return (
+    <div className={`${styles.card} ${props.className}`}>
+      {props.children}
+    </div>
+  )
+}
