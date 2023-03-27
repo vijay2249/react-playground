40739bd6244f2938f31dd1d2a2ff8534b78e3897
VIJAY 'H'CN <65058877+vijay2249@users.noreply.github.com>
2023-03-27 17:05:00 +0530

    simple example for custom hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
-
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  
-//   useEffect(()=>{
-//     const userLoggedIn = localStorage.getItem("userLoggedIn")
-//     if(userLoggedIn === "1") setIsLoggedIn(true)
-//   },[])
-
-//   const loginHandler = (email, password) => {
-//     localStorage.setItem("userLoggedIn", "1")
-//     setIsLoggedIn(true);
-//   };
-
-//   const logoutHandler = () => {
-//     localStorage.removeItem("userLoggedIn")
-//     setIsLoggedIn(false);
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <AuthContext.Provider 
-//         value={{
-//           isLoggedIn: isLoggedIn,
-//           onLogout: logoutHandler
-//         }}
-//       >
-//         <MainHeader/>
-//         <main>
-//           {!isLoggedIn && <Login onLogin={loginHandler} />}
-//           {isLoggedIn && <Home onLogout={logoutHandler} />}
-//         </main>
-//       </AuthContext.Provider>
-//     </React.Fragment>
-//   );
-// }
-
-function App(){
-  const context = useContext(AuthContext)
-  return(
+function App() {
+  return (
     <React.Fragment>
-      <MainHeader/>
-      <main>
-        {!context.isLoggedIn && <Login />}
-        {context.isLoggedIn && <Home />}
-      </main>
+      <ForwardCounter />
+      <BackwardCounter />
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
